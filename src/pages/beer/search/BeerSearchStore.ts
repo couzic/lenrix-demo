1dc41e158d68704614ef8c4ae79b8508ca80ef64
Mikael Couzic <mikael@mikael-thinkpad>
2019-01-23 10:57:00 +0100

    Update dependencies

--- src/pages/beer/search/BeerSearchStore.ts
+++ src/pages/beer/search/BeerSearchStore.ts
@@ -17,27 +17,26 @@ export const createBeerSearchStore = (
       cancelBeerSearch: undefined
     }>()
     .updates(_ => ({
-      searchInputChanged: value => _.setFields({ pending: true }),
+      searchInputChanged: () => _.setFields({ pending: true }),
       receivedBeers: beers => _.setFields({ pending: false, beers }),
       cancelBeerSearch: () => _.setFields({ pending: false, beers: [] })
     }))
-    .epics({
+    .pureEpics({
       searchInputChanged: pipe(
         filter(value => value === ''),
         mapTo({ cancelBeerSearch: undefined })
       )
     })
-    .epics({
-      searchInputChanged: (inputValue$, store) =>
-        inputValue$.pipe(
-          filter(inputValue => inputValue.length > 0),
-          switchMap(inputValue =>
-            beerService
-              .searchBeers(inputValue)
-              .pipe(takeUntil(store.action$.ofType('cancelBeerSearch')))
-          ),
-          map(beers => ({ receivedBeers: beers }))
-        )
-    })
+    .epics(store => ({
+      searchInputChanged: pipe(
+        filter(inputValue => inputValue.length > 0),
+        switchMap(inputValue =>
+          beerService
+            .searchBeers(inputValue)
+            .pipe(takeUntil(store.action$.ofType('cancelBeerSearch')))
+        ),
+        map(beers => ({ receivedBeers: beers }))
+      )
+    }))
 
 export type BeerSearchStore = ReturnType<typeof createBeerSearchStore>
